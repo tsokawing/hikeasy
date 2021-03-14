ce5d87d245a0f03989fed123463d5e4f1885fcb9
Vectorial1024 <vincentwwy168@gmail.com>
2021-03-14 19:17:07 +0800

    Install typeorm + mysql
    Also includes a bit of sample code

---
+++ Backend/src/entity/User.ts
@@ -0,0 +1,21 @@
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity()
+export class User {
+
+    // note: for entity attributes we will use a ! after the name to assert that they are defined
+    // so that TypeScript compiler does not complain about this
+
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    firstName!: string;
+
+    @Column()
+    lastName!: string;
+
+    @Column()
+    age!: number;
+
+}
