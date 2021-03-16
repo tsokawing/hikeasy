c714e93c646744310f69d594b9ac7ef8bc231ffb
Vectorial1024 <vincentwwy168@gmail.com>
2021-03-16 18:23:39 +0800

    Prettier lint

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,30 +1,29 @@
-import {connectTest} from './MysqlConnector'
 import { HikEasyApp } from "./HikEasyApp";
 
 // load dotenv as the first thing to do
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
 
 // establish root endpoint
 // this is a sample on how to further extend the backend code
-app.get('/', async function(req: any, res: any) {
-    res.send({
-        'running': true,
-        'message': 'Hello World!',
-    });
+app.get("/", async function (req: any, res: any) {
+  res.send({
+    running: true,
+    message: "Hello World!",
+  });
 });
 
 // read two number and sum them
-app.get('/demo_sum', async function(req: any, res: any){
-    const firstNumber = req.params.num1;
-    const secondNumber = req.params.num2;
-    const sum = firstNumber + secondNumber;
-    res.send({
-        'result': sum
-    });
+app.get("/demo_sum", async function (req: any, res: any) {
+  const firstNumber = req.params.num1;
+  const secondNumber = req.params.num2;
+  const sum = firstNumber + secondNumber;
+  res.send({
+    result: sum,
+  });
 });
 
 // link the different modules to the main file
@@ -33,5 +32,5 @@ const appInstance = new HikEasyApp();
 
 // finally specify that we are starting the backend
 app.listen(port, () => {
-    console.log(`Backend is now started at http://localhost:${port}`)
-})
+  console.log(`Backend is now started at http://localhost:${port}`);
+});
