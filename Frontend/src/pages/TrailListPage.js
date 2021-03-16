923860b31f5cc04984457939ad2fe3dfb8dd4832
Ka Wing Tso <kawingtsohk@gmail.com>
2021-03-16 17:42:24 +0800

    Display trail items on TrailListPage dynamically

--- Frontend/src/pages/TrailListPage.js
+++ Frontend/src/pages/TrailListPage.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 
-const TrailListPage = () => (
-  <>
-    <h1>Trails Results:</h1>
-    <span>trail1</span>
-    <span>trail1</span>
-  </>
-);
+class TrailListPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      trailList: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("./trails.json")
+      .then((response) => response.json())
+      .then((result) => {
+        const trails = result.map((item) => {
+          return item;
+        });
+        this.setState({ trailList: trails });
+      });
+  }
+
+  render() {
+    const trailItems = this.state.trailList.map((item) => (
+      <>
+        <div>Trail name: {item.trailName}</div>
+        <div>Difficulty: {item.difficulty}</div>
+        <div>Rating: {item.rating}</div>
+        <div>Description: {item.description}</div>
+      </>
+    ));
+
+    return (
+      <>
+        <h1>Trails Results:</h1>
+        {trailItems}
+      </>
+    );
+  }
+}
 
 export default TrailListPage;
