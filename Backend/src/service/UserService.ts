eb5385ff0203cf35554f50de5c22f2d16766a7a5
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2021-03-18 00:30:35 +0800

    Update UserService.ts
    Updating the user info

--- Backend/src/service/UserService.ts
+++ Backend/src/service/UserService.ts
@@ -1,16 +1,18 @@
 import { Application, Request, Response } from 'express';
 import { User } from '../entity/User';
 import { HikEasyApp } from '../HikEasyApp';
+import { EntityManager } from 'typeorm';
 
 export class UserService {
   public constructor(app: Application) {
     app.get('/users/get_all', this.getAllUsers);
     app.post('/users/add_user', this.addNewUsers);
+    app.get('/users/update_user/:UserID', this.updateUsers);
   }
 
   private async getAllUsers(req: Request, res: Response) {
     const users = await HikEasyApp.Instance.EntityManager?.find(User);
-    console.log(users);
+    //console.log(users);
     if (users == undefined) {
       // failed to connect to database
       res.status(503).json({
@@ -80,6 +82,27 @@ export class UserService {
         message: 'Done',
       });
       
+  }
+  //method for updating user info through id
+  private async updateUsers(req: Request, res: Response){
+    console.log(req.param("UserID"));
+    const id = req.params.UserID.split(":");
+    console.log(id[1]);
+    const users = await HikEasyApp.Instance.EntityManager?.findOne(User, id[1]);
+    console.log(users);
+    if(users != undefined){
+        // users.email = req.body['userEmail'];
+        // users.password = req.body['usePassword'];
+        users.email = "gg@gmail.com";
+        users.password = "dsdfsf ";
+    }
+    if(HikEasyApp.Instance.EntityManager != undefined){
+        HikEasyApp.Instance.EntityManager.save(users);
+    }
+    res.json({
+      success: true,
+      message: 'Done',
+    });
   }
 
 }
