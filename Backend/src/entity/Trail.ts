d477a1157b19dee1c65913e091af83c858c532d8
Vectorial1024 <vincentwwy168@gmail.com>
2021-03-17 11:08:14 +0800

    Create Trail.ts

---
+++ Backend/src/entity/Trail.ts
@@ -0,0 +1,29 @@
+import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn} from "typeorm";
+
+@Entity()
+export class Trail {
+
+    @PrimaryGeneratedColumn()
+    trailID!: number;
+
+    @Column()
+    difficulty!: number;
+
+    @Column()
+    trailName!: string;
+
+    @Column()
+    description!: string;
+
+    @Column()
+    isVerified!: boolean;
+
+    @CreateDateColumn()
+    createdAt!: Date;
+
+    @UpdateDateColumn()
+    updatedAt!: Date;
+    
+    @DeleteDateColumn()
+    deletedAt?: Date;
+}
