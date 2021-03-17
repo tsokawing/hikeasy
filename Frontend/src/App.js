ff489a6de0fa35ce4917cfcc9fb974e07e5a905a
icanwhistle <40556186+icanwhistle@users.noreply.github.com>
2021-03-17 12:01:47 +0800

    Add Footer to frontend
    Add footer

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,6 +6,7 @@ import EventListPage from "./pages/EventListPage";
 import LoginPage from "./pages/LoginPage";
 import NavBar from "./components/NavBar";
 import NotFoundPage from "./pages/NotFoundPage";
+import FooterPro from "./components/FooterPro";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
             <Route component={NotFoundPage} />
           </Switch>
         </div>
+        <FooterPro />
       </div>
     </Router>
   );
