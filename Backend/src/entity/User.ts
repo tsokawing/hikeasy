83002622fd7f6c8795ac34868281e979f6dcf293
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2021-03-18 23:37:42 +0800

    Update the foreign key for user's events

--- Backend/src/entity/User.ts
+++ Backend/src/entity/User.ts
@@ -1,4 +1,5 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, OneToMany} from "typeorm";
+import {Event} from "./Event";
 
 @Entity()
 export class User {
@@ -28,5 +29,8 @@ export class User {
     })
     password!: string;
 
+    @OneToMany(() => Event, event => event.user)
+    events: Event[] | undefined;
+    
 
 }
