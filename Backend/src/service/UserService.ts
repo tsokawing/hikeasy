2d7ca47491f0e1d663b66966125b4cdf275ddac9
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2021-03-18 23:34:10 +0800

    Update UserService.ts
    Updated the updateUser

--- Backend/src/service/UserService.ts
+++ Backend/src/service/UserService.ts
@@ -7,7 +7,7 @@ export class UserService {
   public constructor(app: Application) {
     app.get('/users/get_all', this.getAllUsers);
     app.post('/users/add_user', this.addNewUsers);
-    app.get('/users/update_user/:UserID', this.updateUsers);
+    app.post('/users/update_user/:userID', this.updateUsers);
   }
 
   private async getAllUsers(req: Request, res: Response) {
@@ -23,6 +23,7 @@ export class UserService {
       res.status(200).json(users);
     }
   }
+
   private async addNewUsers(req: Request, res: Response){
       const users = new User();
       users.firstName = req.body['userFirstname'];
@@ -66,16 +67,6 @@ export class UserService {
       }
       //check whether the email have been registered 
       const temp = await HikEasyApp.Instance.EntityManager?.find(User);
-      //test duplicate email? 
-    //   temp.forEach(function(temp){
-    //     if(users.email == temp.email){
-    //         res.json({
-    //             //success: false,
-    //             message: 'Duplicate Email',
-
-    //         });
-    //     }
-    //   });
       HikEasyApp.Instance.EntityManager.save(users);
       res.json({
         success: true,
@@ -85,16 +76,13 @@ export class UserService {
   }
   //method for updating user info through id
   private async updateUsers(req: Request, res: Response){
-    console.log(req.param("UserID"));
-    const id = req.params.UserID.split(":");
-    console.log(id[1]);
-    const users = await HikEasyApp.Instance.EntityManager?.findOne(User, id[1]);
+    const id = parseInt(req.params['userID']);
+    //console.log(id);
+    const users = await HikEasyApp.Instance.EntityManager?.findOne(User, id);
     console.log(users);
     if(users != undefined){
-        // users.email = req.body['userEmail'];
-        // users.password = req.body['usePassword'];
         users.email = "gg@gmail.com";
-        users.password = "dsdfsf ";
+        users.password = "test";
     }
     if(HikEasyApp.Instance.EntityManager != undefined){
         HikEasyApp.Instance.EntityManager.save(users);
@@ -104,5 +92,4 @@ export class UserService {
       message: 'Done',
     });
   }
-
 }
