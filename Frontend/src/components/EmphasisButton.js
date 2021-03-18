72720fd21c4c1b5473759eb2d6e66a3f556767a8
icanwhistle <40556186+icanwhistle@users.noreply.github.com>
2021-03-18 22:21:57 +0800

    Button version-2

--- Frontend/src/components/EmphasisButton.js
+++ Frontend/src/components/EmphasisButton.js
@@ -3,7 +3,7 @@ import { Button } from 'semantic-ui-react'
 
 const EmphasisButton = () => (
   <div>
-    <Button secondary>New</Button>
+    <Button size = 'mini' secondary>New</Button>
   </div>
 )
 
