d079e33d02a874f41cbf0720cf37b4f2bfc5f922
Vectorial1024 <vincentwwy168@gmail.com>
2021-03-23 22:18:52 +0800

    Update more the script

--- Backend/prod_server_start.sh
+++ Backend/prod_server_start.sh
@@ -12,16 +12,16 @@ echo "Stopping the correct PM2 process..."
 pm2 stop build/index.js
 
 current_timestamp=$(date +'%Y%m%d_%H%M%S')
-outputlog_name="foreverlogs/${current_timestamp}_out.log"
-errorlog_name="foreverlogs/${current_timestamp}_err.log"
+#outputlog_name="foreverlogs/${current_timestamp}_out.log"
+#errorlog_name="foreverlogs/${current_timestamp}_err.log"
 pm2log_name="pm2_logs/${current_timestamp}.log"
 
-echo "Starting a new PM2 process..."
+echo "Starting the backend via PC2..."
 pm2 start build/index.js --time --log pm2log_name
 #forever start -o $outputlog_name -e $errorlog_name build/index.js
 
 #See if we started the forever process successfully:
-sleep 5
-echo "Should have started a new PM2 process. Listing running forever processes:"
+#sleep 5
+#echo "Should have started a new PM2 process. Listing running PM2 processes:"
 #forever list
-pm2 ls
+#pm2 ls
