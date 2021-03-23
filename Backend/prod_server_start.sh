c6a58a22d99e66a3abd64365c02a43cd468fb749
Vectorial1024 <vincentwwy168@gmail.com>
2021-03-23 22:07:53 +0800

    Try another daemon manager

--- Backend/prod_server_start.sh
+++ Backend/prod_server_start.sh
@@ -8,17 +8,20 @@ echo "> npm install"
 npm install
 echo "Compiling TypeScript..."
 tsc
-echo "Stopping the correct forever process..."
-forever stop build/index.js
+echo "Stopping the correct PM2 process..."
+pm2 stop build/index.js
 
 current_timestamp=$(date +'%Y%m%d_%H%M%S')
 outputlog_name="foreverlogs/${current_timestamp}_out.log"
 errorlog_name="foreverlogs/${current_timestamp}_err.log"
+pm2log_name="pm2_logs/${current_timestamp}.log"
 
-echo "Starting a new forever process..."
-forever start -o $outputlog_name -e $errorlog_name build/index.js
+echo "Starting a new PM2 process..."
+pm2 start build/index.js --time --log pm2log_name
+#forever start -o $outputlog_name -e $errorlog_name build/index.js
 
 #See if we started the forever process successfully:
 sleep 5
-echo "Should have started a new forever process. Listing running forever processes:"
-forever list
+echo "Should have started a new PM2 process. Listing running forever processes:"
+#forever list
+pm2 ls
