e1235b3b5f62e8a1e6005a5d36ebee47f911c4ec
Vectorial1024 <vincentwwy168@gmail.com>
2021-03-23 21:41:12 +0800

    Update the server starting script

--- Backend/prod_server_start.sh
+++ Backend/prod_server_start.sh
@@ -8,15 +8,15 @@ echo "> npm install"
 npm install
 echo "Compiling TypeScript..."
 tsc
-echo "Stopping forever processes..."
-forever stopall
+echo "Stopping the correct forever process..."
+forever stop build/index.js
 
 current_timestamp=$(date +'%Y%m%d_%H%M%S')
 outputlog_name="foreverlogs/${current_timestamp}_out.log"
 errorlog_name="foreverlogs/${current_timestamp}_err.log"
 
 echo "Starting a new forever process..."
-forever start -c "node --max-old-space-size=8192" -o $outputlog_name -e $errorlog_name build/index.js
+forever start -o $outputlog_name -e $errorlog_name build/index.js
 
 #See if we started the forever process successfully:
 sleep 1
