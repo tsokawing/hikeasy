97f7eb646d0fccf0313ee6949dbf325d5b30d41b
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2021-03-30 15:24:28 +0800

    Add Trailpage

---
+++ Frontend/src/pages/TrailPage.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+const TrailPage = () => (
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     trailList: [],
+  //   };
+  // }
+
+  // componentDidMount() {
+  //   fetch("http://localhost:8080/trails/get_all")
+  //     .then((response) => response.json())
+  //     .then((result) => {
+  //       const trails = result.map((item) => {
+  //         console.log(item);
+  //         return item;
+  //       });
+  //       this.setState({ trailList: trails });
+  //     });
+  // }
+  <>
+    <div>Trail Page.</div>
+    <h4>sdf sdhfhdsfsfdsf sdfsd</h4>
+    <h5>fswf sdfsdfsdfsdfsdfsd</h5>
+  </>
+);
+
+export default TrailPage;
