4ff8da06fff74fb4f1ed5d2a9a0051497d6b7ea7
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2021-03-30 15:23:24 +0800

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import TrailPage from "./pages/TrailPage";
 import TrailListPage from "./pages/TrailListPage";
 import EventListPage from "./pages/EventListPage";
 import LoginPage from "./pages/LoginPage";
 import NavBar from "./components/NavBar";
 import NotFoundPage from "./pages/NotFoundPage";
 import FooterComponent from "./components/FooterComponent";
+
+// import TrailPage from "./pages/TrailPage";
 
 function App() {
   return (
@@ -19,6 +22,7 @@ function App() {
             <Route path="/trails/" component={TrailListPage} exact />
             <Route path="/events/" component={EventListPage} exact />
             <Route path="/login/" component={LoginPage} exact />
+            <Route path="/trail/" component={TrailPage} exact />
             <Route component={NotFoundPage} />
           </Switch>
         </div>
