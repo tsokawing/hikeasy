6ef0fb3e389180d51dc5e5827c33cfd7d5d78556
Ka Wing Tso <kawingtsohk@gmail.com>
2021-04-02 17:09:16 +0800

    Create trail info section in image section
    Use rating component from material UI.

--- Frontend/src/components/ImageSection.js
+++ Frontend/src/components/ImageSection.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "../App.css";
 import { ImageButton } from "./ImageButton";
 import "./ImageSection.css";
+import Rating from "@material-ui/lab/Rating";
 
 class ImageSection extends Component {
   render() {
@@ -13,28 +14,40 @@ class ImageSection extends Component {
       <div className="image-container">
         <h1>{trail ? trail.name : null}</h1>
         <p>{trail ? trail.description : null}</p>
-        <div className="image-btns">
-          <ImageButton
-            className="image-btns"
-            buttonStyle="image-btn--outline"
-            buttonSize="image-btn--large"
-          >
-            LIKE
-          </ImageButton>
-          <ImageButton
-            className="image-btns"
-            buttonStyle="image-btn--outline"
-            buttonSize="image-btn--large"
-          >
-            SHARE
-          </ImageButton>
-          <ImageButton
-            className="image-btns"
-            buttonStyle="image-btn--outline"
-            buttonSize="image-btn--large"
-          >
-            RATE
-          </ImageButton>
+        <div className="image-btn-container">
+          <div className="trail-info-container">
+            <Rating
+              name="read-only"
+              value={trail ? trail.difficulty : 0}
+              size="large"
+              readOnly
+            />
+            <p class="trail-info">Shatin, NT 28KM</p>
+          </div>
+
+          <div className="image-btns">
+            <ImageButton
+              className="image-btns"
+              buttonStyle="image-btn--outline"
+              buttonSize="image-btn--large"
+            >
+              LIKE
+            </ImageButton>
+            <ImageButton
+              className="image-btns"
+              buttonStyle="image-btn--outline"
+              buttonSize="image-btn--large"
+            >
+              SHARE
+            </ImageButton>
+            <ImageButton
+              className="image-btns"
+              buttonStyle="image-btn--outline"
+              buttonSize="image-btn--large"
+            >
+              RATE
+            </ImageButton>
+          </div>
         </div>
       </div>
     );
