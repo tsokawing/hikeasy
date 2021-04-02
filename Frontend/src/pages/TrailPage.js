8242f4b69d6d4c120b5252ba7118da799caa360b
Ka Wing Tso <kawingtsohk@gmail.com>
2021-04-02 14:50:50 +0800

    Dynamically direct to TrailPage

--- Frontend/src/pages/TrailPage.js
+++ Frontend/src/pages/TrailPage.js
@@ -1,30 +1,39 @@
 import React, { Component } from "react";
 
 class TrailPage extends Component {
-
   constructor() {
     super();
     this.state = {
-      trailList: []
+      trailList: [],
     };
   }
 
   componentDidMount() {
     // allow cors to fetch ==> install cors extension for chrome
 
-    var get_all = "http://ec2-18-188-120-239.us-east-2.compute.amazonaws.com:8080/trails/get_all/"
+    var get_all =
+      "http://ec2-18-188-120-239.us-east-2.compute.amazonaws.com:8080/trails/get_all/";
 
-    var str1 =  "http://ec2-18-188-120-239.us-east-2.compute.amazonaws.com:8080/trails/get_specific/";
-    var id = this.props.match.params.trailID
+    var str1 =
+      "http://ec2-18-188-120-239.us-east-2.compute.amazonaws.com:8080/trails/get_specific/";
+    var id = this.props.match.params.trailID;
     var link = str1.concat(id);
     fetch(get_all)
       .then((response) => response.json())
       .then((result) => {
-        const trails = result.map((item) => {
-            if (item.id == id) {
-              return item;
-            }
+        const trails = result.filter((item) => {
+          if (item.id == id) {
+            return item;
+          }
         });
+
+        // const trails = result.map((item) => {
+        //   if (item.id == id) {
+        //     return item;
+        //   } else {
+        //     return null;
+        //   }
+        // });
         this.setState({ trailList: trails });
       });
   }
@@ -35,7 +44,11 @@ class TrailPage extends Component {
     }
 
     return (
-      <div>TEST</div>
+      <div>
+        {this.state.trailList.length > 0
+          ? this.state.trailList[0].id
+          : "LOADING"}
+      </div>
     );
   }
 }
