8a1bd8d74053ce3c69dbfa63bb8835c4e3d490e5
Ka Wing Tso <kawingtsohk@gmail.com>
2021-04-04 15:13:46 +0800

    Install leaflet, add map in trail page
    Co-Authored-By: briansodenkin <66714154+briansodenkin@users.noreply.github.com>

--- Frontend/src/pages/TrailPage.js
+++ Frontend/src/pages/TrailPage.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import ImageSection from "../components/ImageSection";
 import Comments from "../components/Comments";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./TrailPage.css";
 
 class TrailPage extends Component {
   constructor() {
@@ -18,7 +21,7 @@ class TrailPage extends Component {
       "http://ec2-18-188-120-239.us-east-2.compute.amazonaws.com:8080/trails/get_all/";
 
     var id = this.props.match.params.trailID;
-    var get_review = "http://localhost:8080/review/get_all_by_trail/".concat(
+    var get_review = "http://ec2-18-188-120-239.us-east-2.compute.amazonaws.com:8080/review/get_all_by_trail/".concat(
       id
     );
 
@@ -50,6 +53,24 @@ class TrailPage extends Component {
       <>
         <ImageSection trail={this.state.trailList[0]} />
         <Comments reviews={this.state.reviewList} />
+
+        <div className={"trail-map"}>
+          <MapContainer
+            center={[51.505, -0.09]}
+            zoom={12}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
       </>
     );
   }
