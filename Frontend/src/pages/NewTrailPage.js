3758986c66b3327f85732380c0c95275f5a78467
Ka Wing Tso <kawingtsohk@gmail.com>
2021-04-08 13:21:58 +0800

    Touch up NewTrailPage styles
    Co-Authored-By: briansodenkin <66714154+briansodenkin@users.noreply.github.com>

--- Frontend/src/pages/NewTrailPage.js
+++ Frontend/src/pages/NewTrailPage.js
@@ -3,6 +3,8 @@ import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import MenuItem from "@material-ui/core/MenuItem";
 import UploadImages from "../components/UploadImages";
+import MapSection from "../components/MapSection";
+import "./NewTrailPage.css";
 
 const difficulties = [
   {
@@ -47,63 +49,74 @@ class NewTrailPage extends Component {
   render() {
     return (
       <>
-        <div>
-          <TextField
-            required
-            id="standard-required"
-            inputRef={(c) => {
-              this.state.title = c;
-            }}
-            label="Required"
-            defaultValue="Title"
-          />
-          <TextField
-            required
-            id="standard-required"
-            inputRef={(c) => {
-              this.state.description = c;
-            }}
-            label="Required"
-            defaultValue="Description"
-          />
-          <TextField
-            required
-            id="standard-required"
-            inputRef={(c) => {
-              this.state.length = c;
-            }}
-            label="Required"
-            defaultValue="Length"
-          />
-          <TextField
-            required
-            id="standard-required"
-            inputRef={(c) => {
-              this.state.city = c;
-            }}
-            label="Required"
-            defaultValue="City"
-          />
-          <TextField
-            id="standard-select-difficulty"
-            select
-            label="Select"
-            value={this.difficulty}
-            inputRef={(c) => {
-              this.state.difficulty = c;
-            }}
-            onChange={this.clicked}
-            helperText="Please select your difficulty"
-          >
-            {difficulties.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
+        <div className="newtrailpage-container">
+          <div className="newtrail-info-section">
+            <div className={"trail-map"}>
+              <MapSection />
+            </div>
+            <div className="info-section-right">
+              <div className="newtrail-details">
+                <TextField
+                  required
+                  id="standard-required"
+                  inputRef={(c) => {
+                    this.state.title = c;
+                  }}
+                  label="Required"
+                  defaultValue="Title"
+                />
+                <TextField
+                  required
+                  id="standard-required"
+                  inputRef={(c) => {
+                    this.state.description = c;
+                  }}
+                  label="Required"
+                  defaultValue="Description"
+                />
+                <TextField
+                  required
+                  id="standard-required"
+                  inputRef={(c) => {
+                    this.state.length = c;
+                  }}
+                  label="Required"
+                  defaultValue="Length"
+                />
+                <TextField
+                  required
+                  id="standard-required"
+                  inputRef={(c) => {
+                    this.state.city = c;
+                  }}
+                  label="Required"
+                  defaultValue="City"
+                />
+                <TextField
+                  id="standard-select-difficulty"
+                  select
+                  label="Select"
+                  value={this.difficulty}
+                  inputRef={(c) => {
+                    this.state.difficulty = c;
+                  }}
+                  onChange={this.clicked}
+                  helperText="Please select your difficulty"
+                >
+                  {difficulties.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </div>
+              <div className="image-section">
+                <UploadImages />
+              </div>
+            </div>
+          </div>
+          <div onClick={this.clicked}>new trail page</div>
         </div>
-        <UploadImages />
-        <div onClick={this.clicked}>new trail page</div>
       </>
     );
   }
