ccc70d7afd7e59ac1d901e70fb3a8e157d10d044
Ka Wing Tso <kawingtsohk@gmail.com>
2021-04-11 21:37:38 +0800

    Update firebase config credentials

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -1,16 +1,14 @@
-import firebase from 'firebase';
-import * as firebaseui from 'firebaseui';
+import firebase from "firebase";
+import * as firebaseui from "firebaseui";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAoXEIkPzrDE3oOyyKqnN6r1WU2bIpqm2U",
-    authDomain: "hikeasy-4b1dd.firebaseapp.com",
-    projectId: "hikeasy-4b1dd",
-    storageBucket: "hikeasy-4b1dd.appspot.com",
-    messagingSenderId: "197760013635",
-    appId: "1:197760013635:web:c964c7b34728f1598d0bf6",
-    measurementId: "G-71JM5L06RQ"
-
-   
+  apiKey: "AIzaSyAoXEIkPzrDE3oOyyKqnN6r1WU2bIpqm2U",
+  authDomain: "hikeasy-4b1dd.firebaseapp.com",
+  projectId: "hikeasy-4b1dd",
+  storageBucket: "hikeasy-4b1dd.appspot.com",
+  messagingSenderId: "197760013635",
+  appId: "1:197760013635:web:c964c7b34728f1598d0bf6",
+  measurementId: "G-71JM5L06RQ",
 };
 
 firebase.initializeApp(firebaseConfig);
