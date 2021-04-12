fa6b46307de24a3185f9b607ec4c12a2ac46fd6c
Vectorial1024 <vincentwwy168@gmail.com>
2021-04-12 12:52:10 +0800

    Add new endpoint for login/register double use

--- Backend/src/service/UserService.ts
+++ Backend/src/service/UserService.ts
@@ -1,13 +1,18 @@
 import { Application, Request, Response } from 'express';
 import { User } from '../entity/User';
 import { HikEasyApp } from '../HikEasyApp';
-import { EntityManager } from 'typeorm';
 import { ResponseUtil } from '../util/ResponseUtil';
+import { FirebaseAuthenticator } from '../FirebaseAuthenticator';
 
 export class UserService {
   public constructor(app: Application) {
     app.get('/users/get_all', this.getAllUsers);
     app.post('/users/add_user', this.addNewUsers);
+    app.post(
+      '/users/login_or_register/',
+      FirebaseAuthenticator.authenticate,
+      this.loginOrRegisterUser
+    );
     app.post('/users/update_user/:userID', this.updateUsers);
   }
 
@@ -23,52 +28,71 @@ export class UserService {
     }
   }
 
-  private async addNewUsers(req: Request, res: Response){
-      const users = new User();
-      users.firstName = req.body['userFirstname'];
-      users.lastName = req.body['userLastname'];
-      users.age = req.body['userAge'];
-      users.email = req.body['userEmail'];
-      users.password = req.body['userPassword'];
-      if (users.firstName === undefined || users.lastName == undefined) {
-        res.json({
-          success: false,
-          message: 'Missing User Name',
-        });
-        return;
-      }
-      if (users.age === undefined) {
-          users.age = 0;
-        res.json({
-          message: 'Missing user age',
-        });
-      }
-      if (users.email === undefined) {
-        res.json({
-          success: false,
-          message: 'Missing email address',
-        });
-        return;
-      }
-      if (users.password === undefined) {
-        res.json({
-          success: false,
-          message: 'Missing password',
-        });
-        return;
-      }
-      if (HikEasyApp.Instance.EntityManager == undefined) {
-        ResponseUtil.respondWithDatabaseUnreachable(res);
-        return;
-      }
-      //check whether the email have been registered 
-      const temp = await HikEasyApp.Instance.EntityManager?.find(User);
-      HikEasyApp.Instance.EntityManager.save(users);
+  private async loginOrRegisterUser(req: Request, res: Response) {
+    // need to check whether we are existing user login or new user register
+    const requestedUser = await FirebaseAuthenticator.extractProperUserFromAuth(
+      req
+    );
+    if (requestedUser !== undefined) {
+      // existing user, also passed firebase
       res.json({
         success: true,
-        message: 'Done',
+        message: 'OK, welcome!',
       });
-      
+      return;
+    } else {
+      // new user, already passed firebase
+      await this.addNewUsers(req, res);
+    }
+  }
+
+  private async addNewUsers(req: Request, res: Response) {
+    const users = new User();
+    users.firstName = req.body['userFirstname'];
+    users.lastName = req.body['userLastname'];
+    users.age = req.body['userAge'];
+    users.email = req.body['userEmail'];
+    users.password = req.body['userPassword'];
+    if (users.firstName === undefined || users.lastName == undefined) {
+      res.json({
+        success: false,
+        message: 'Missing User Name',
+      });
+      return;
+    }
+    if (users.age === undefined) {
+      users.age = 0;
+      res.json({
+        success: false,
+        message: 'Missing user age',
+      });
+      return;
+    }
+    if (users.email === undefined) {
+      res.json({
+        success: false,
+        message: 'Missing email address',
+      });
+      return;
+    }
+    if (users.password === undefined) {
+      res.json({
+        success: false,
+        message: 'Missing password',
+      });
+      return;
+    }
+    if (HikEasyApp.Instance.EntityManager == undefined) {
+      ResponseUtil.respondWithDatabaseUnreachable(res);
+      return;
+    }
+    //check whether the email have been registered
+    const temp = await HikEasyApp.Instance.EntityManager?.find(User);
+    HikEasyApp.Instance.EntityManager.save(users);
+    res.json({
+      success: true,
+      message: 'Done',
+    });
   }
   //method for updating user info through id
   private async updateUsers(req: Request, res: Response){
