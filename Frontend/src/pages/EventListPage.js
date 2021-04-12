a706c6d7c2917cbe9fcb627af1eb1a78ffea6c70
Ka Wing Tso <kawingtsohk@gmail.com>
2021-04-12 23:58:24 +0800

    Create card grid in event list page
    Co-Authored-By: Vincent Wong <vincentwwy168@gmail.com>
    Co-Authored-By: briansodenkin <66714154+briansodenkin@users.noreply.github.com>

--- Frontend/src/pages/EventListPage.js
+++ Frontend/src/pages/EventListPage.js
@@ -3,23 +3,27 @@ import SearchBarComponent from "../components/SearchBarComponent";
 import TrailList from "../components/TrailList";
 import EmphasisButton from "../components/EmphasisButton";
 import "./EventListPage.css";
+import { SpringGrid } from "react-stonecutter";
+import EventCard from "../components/EventCard";
+
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 class EventListPage extends Component {
   constructor() {
     super();
     this.state = {
-      trailList: [],
+      eventList: [],
     };
   }
 
   componentDidMount() {
-    fetch("./trails.json")
+    fetch("http://localhost:8080/events/get_all")
       .then((response) => response.json())
       .then((result) => {
-        const trails = result.map((item) => {
+        const events = result.map((item) => {
           return item;
         });
-        this.setState({ trailList: trails });
+        this.setState({ eventList: events }, () => {});
       });
   }
 
@@ -36,7 +40,27 @@ class EventListPage extends Component {
         </div>
 
         <SearchBarComponent />
-        <TrailList trailList={this.state.trailList} />
+        <div className="grid-container">
+          <SpringGrid
+            component="ul"
+            columns={3}
+            columnWidth={350}
+            gutterWidth={50}
+            // gutterHeight={1}
+            itemHeight={350}
+            // springConfig={{ stiffness: 170, damping: 26 }}
+          >
+            {this.state.eventList.map((item) => (
+              <div>
+                <Link to={`../trail/${item.id}`}>
+                  <EventCard />
+                </Link>
+              </div>
+            ))}
+          </SpringGrid>
+        </div>
+
+        {/* <TrailList trailList={this.state.trailList} /> */}
       </>
     );
   }
