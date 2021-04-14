2ca3a198412cac3af46e14cf3090384436b5ec6d
Ka Wing Tso <kawingtsohk@gmail.com>
2021-04-14 22:35:16 +0800

    Add clear button for map in new trail page
    Co-Authored-By: Vincent Wong <vincentwwy168@gmail.com>
    Co-Authored-By: briansodenkin <66714154+briansodenkin@users.noreply.github.com>

--- Frontend/src/components/MapSection.js
+++ Frontend/src/components/MapSection.js
@@ -1,4 +1,5 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
 import {
   MapContainer,
   TileLayer,
@@ -18,14 +19,17 @@ const icon = L.icon({
   shadowUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png",
 });
 
-function MyComponent({ saveMarkers }) {
+let mapInstanceReference;
+
+function MyComponent({ markerAppender }) {
   const map = useMapEvents({
     click: (e) => {
       const { lat, lng } = e.latlng;
-      L.marker([lat, lng], { icon }).addTo(map);
-      saveMarkers([lat, lng]);
+      markerAppender(lat, lng);
     },
   });
+  mapInstanceReference = map;
+
   return null;
 }
 
@@ -34,9 +38,11 @@ class MapSection extends Component {
     super(props);
     this.state = {
       markers: [[40.7, -74]],
+      markerRefs: [],
       point: [],
       center: [],
     };
+    this.mapRef = React.createRef();
   }
 
   componentDidMount() {
@@ -44,42 +50,65 @@ class MapSection extends Component {
       var polyline = require("@mapbox/polyline");
       let waypoints = polyline.decode(this.props.trail.waypoints, 6);
       let displayCenter = this.props.trail.displayCenter;
-      console.log(displayCenter);
       this.setState({ point: waypoints });
       this.setState({ center: displayCenter });
     }
   }
+
+  appendNewPointToMap = (lat, lng) => {
+    // remember this marker
+    let markerRef = L.marker([lat, lng], { icon }).addTo(mapInstanceReference);
+    let newMarkerArray = [...this.state.markers, markerRef];
+    this.setState({ markers: newMarkerArray });
+
+    // and also remember this point
+    const point = [...this.state.point, [lat, lng]];
+    this.setState((prevState) => ({ ...prevState, point }));
+  };
 
   saveMarkers = (newMarkerCoords) => {
     const point = [...this.state.point, newMarkerCoords];
     this.setState((prevState) => ({ ...prevState, point }));
   };
 
+  clearAllWaypointsOnMap = () => {
+    // remove all markers first
+    this.state.markers.map((marker) => {
+      mapInstanceReference.removeLayer(marker);
+    });
+    this.setState({ markers: [] });
+    this.setState({ point: [] });
+    // do not move the center, let the user move it
+  };
+
   render() {
     return (
-      <MapContainer
-        className="Map"
-        center={
-          this.state.center?.length > 0
-            ? this.state.center
-            : [22.28552, 114.15769]
-        }
-        zoom={15}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank" class="jawg-attrib">© <b>Jawg</b>Maps</a> | <a href="https://www.openstreetmap.org/copyright" title="OpenStreetMap is open data licensed under ODbL" target="_blank" class="osm-attrib">© OSM contributors</a>'
-          url="https://{s}.tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-token=XizVS9BuQftVgJAFcw6YteSaCP9OyCzJIYI6vqXeFWQjo7EbAaeYfIVc34YfUWzZ"
-        />
-        <MyComponent saveMarkers={this.saveMarkers} />
-        {this.state.point.map((item) => (
-          <Marker position={[item[0], item[1]]} icon={icon} />
-        ))}
-        <Polyline
-          pathOptions={{ color: "#9b3675" }}
-          positions={this.state.point}
-        />
-      </MapContainer>
+      <>
+        <MapContainer
+          className="Map"
+          center={
+            this.state.center?.length > 0
+              ? this.state.center
+              : [22.28552, 114.15769]
+          }
+          zoom={15}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank" class="jawg-attrib">© <b>Jawg</b>Maps</a> | <a href="https://www.openstreetmap.org/copyright" title="OpenStreetMap is open data licensed under ODbL" target="_blank" class="osm-attrib">© OSM contributors</a>'
+            url="https://{s}.tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-token=XizVS9BuQftVgJAFcw6YteSaCP9OyCzJIYI6vqXeFWQjo7EbAaeYfIVc34YfUWzZ"
+          />
+          <MyComponent markerAppender={this.appendNewPointToMap} />
+          {this.state.point.map((item) => (
+            <Marker position={[item[0], item[1]]} icon={icon} />
+          ))}
+          <Polyline
+            pathOptions={{ color: "#9b3675" }}
+            positions={this.state.point}
+          />
+        </MapContainer>
+        <Button onClick={this.clearAllWaypointsOnMap}>Clear Waypoints</Button>
+      </>
     );
   }
 }
