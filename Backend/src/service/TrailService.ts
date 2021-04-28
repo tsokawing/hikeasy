d1f98e5a334fc45d09d4bd151ea5839e46afec9b
Vectorial1024 <vincentwwy168@gmail.com>
2021-04-28 22:14:10 +0800

    Delete testing code

--- Backend/src/service/TrailService.ts
+++ Backend/src/service/TrailService.ts
@@ -33,11 +33,6 @@ export class TrailService {
     app.get('/trails/get_photo/:fileName', this.returnPhotoWithFileName);
     app.get('/trails/get_photo', this.returnPhotoButThereIsNoGivenFileName);
     // app.post('/trails/delete_photo');
-
-    // app.get('/trails/fake_add', this.testFakeAddTrail);
-    // app.get('/trails/search_test', this.searchSomeTrailTest);
-    // app.post('/trails/post_test', this.postTest);
-    // this.testtest();
   }
 
   private async getAllTrails(req: Request, res: Response) {
@@ -579,44 +574,5 @@ export class TrailService {
     res: Response
   ) {
     ResponseUtil.respondWithError(res, 'Missing file name');
-  }
-
-  private async searchSomeTrailTest(req: Request, res: Response) {
-    console.log(req.query['a']);
-    res.json({});
-  }
-
-  private async postTest(req: Request, res: Response) {
-    console.log('query');
-    console.log(req.query);
-    console.log('body');
-    console.log(req.body);
-    res.json({});
-  }
-
-  private async testFakeAddTrail(req: Request, res: Response) {
-    console.log('I receive fake add request');
-    const trail = new Trail();
-    trail.difficulty = 4;
-    trail.name = 'fake trailk';
-    trail.description = '';
-    trail.isVerified = false;
-    const val = HikEasyApp.Instance?.EntityManager?.save(trail);
-    console.log(val);
-    res.json({});
-  }
-
-  private async testtest() {
-    await this.testEmptyInsert();
-  }
-
-  private async testEmptyInsert() {
-    const trail = new Trail();
-    trail.difficulty = 4;
-    trail.name = 'Dragon Back';
-    trail.description = '';
-    trail.isVerified = false;
-    const value = await HikEasyApp.Instance?.EntityManager?.save(trail);
-    console.log(value);
   }
 }
