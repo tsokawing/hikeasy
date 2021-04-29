8a29fb3832925d4ddbc6c9ff356ec1c3a3c958ec
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2021-04-30 07:41:35 +0800

    Comment Chat.ts

--- Backend/src/entity/User.ts
+++ Backend/src/entity/User.ts
@@ -1,3 +1,10 @@
+/*
+  What: This is used to initialize the User table in the HikEasy database
+  Who: Wong Wing Yan 1155125194
+  Where: backend database
+  Why: to construct the table for storing photos
+  How: use typeorm to connect to mysql database, and set up table for the users
+*/
 import {
   Entity,
   PrimaryGeneratedColumn,
@@ -12,6 +19,7 @@ import { Photo } from './Photo';
 
 @Entity()
 export class User {
+  // attribute of the User table
   // note: for entity attributes we will use a ! after the name to assert that they are defined
   // so that TypeScript compiler does not complain about this
 
@@ -39,16 +47,20 @@ export class User {
     default: '',
   })
   password?: string;
-
+  
+  //one user can post review
   @OneToMany(() => Review, (review) => review.user)
   reviews!: Review[];
 
+  //one user can create many events
   @ManyToMany(() => Event, (event) => event.participants)
   events!: Event[];
 
+  //one user can post many photos
   @OneToMany(() => Photo, (photo) => photo.user)
   photos!: Photo[];
 
+  //one user can have many chats
   @OneToMany(() => Chat, (chat) => chat.user)
   chats!: Chat[];
 }
