e8e03ee19335cb2e585a5c01a100c27707abcdcb
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2021-04-30 07:28:35 +0800

    Comment the Chat.ts

--- Backend/src/entity/Chat.ts
+++ Backend/src/entity/Chat.ts
@@ -1,3 +1,10 @@
+/*
+  What: This is used to initialize the Chat table in the HikEasy database
+  Who: Tsang Tsz Kin Brian 1155126813
+  Where: backend database
+  Why: to construct the table for storing chats
+  How: use typeorm to connect to mysql database, and set up table
+*/
 import {
     Entity,
     PrimaryGeneratedColumn,
@@ -18,10 +25,12 @@ import {
      */
     @PrimaryGeneratedColumn()
     id!: number;
-  
+
+    //each chat have one user
     @ManyToOne(() => User, (user) => user.reviews, { cascade: true })
     user!: User;
   
+    //each chat have one event
     @ManyToOne(() => Event, (event) => event.chats, { cascade: true })
     event!: Event;
 
