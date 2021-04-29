392c2c366f9214190b6fb5c2a2ca40c80508b049
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2021-04-30 07:34:34 +0800

    Comment photo.ts

--- Backend/src/entity/Photo.ts
+++ Backend/src/entity/Photo.ts
@@ -1,3 +1,11 @@
+
+/*
+  What: This is used to initialize the Phtot table in the HikEasy database
+  Who: Wong Wing Yan 1155125194
+  Where: backend database
+  Why: to construct the table for storing photos
+  How: use typeorm to connect to mysql database, and set up table for the photos
+*/
 import {
   Entity,
   PrimaryGeneratedColumn,
@@ -12,6 +20,8 @@ import { Event } from './Event';
 
 @Entity()
 export class Photo {
+  
+  //attributes of the photo table
   @PrimaryGeneratedColumn()
   id!: number;
 
@@ -22,13 +32,16 @@ export class Photo {
 
   @CreateDateColumn()
   createdAt!: Date;
-
+  
+  //one user can post multiple photo
   @ManyToOne(() => User, (user) => user.photos)
   user!: User | undefined;
-
+  
+  //one trail can post multiple photo
   @ManyToOne(() => Trail, (trail) => trail.photos)
   trail!: Trail | undefined;
 
+  //one event can post multiple photo
   @ManyToOne(() => Event, (event) => event.photos)
   event!: Event | undefined;
 }
