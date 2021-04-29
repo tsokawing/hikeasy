428bfeb323784e798e5ec986f2bd279792f296e9
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2021-04-30 07:36:42 +0800

    Comment Review.ts

--- Backend/src/entity/Review.ts
+++ Backend/src/entity/Review.ts
@@ -1,3 +1,10 @@
+/*
+  What: This is used to initialize the Review table in the HikEasy database
+  Who: Wong Wing Yan 1155125194
+  Where: backend database
+  Why: to construct the table for storing photos
+  How: use typeorm to connect to mysql database, and set up table for the reviews
+*/
 import {
   Entity,
   PrimaryGeneratedColumn,
@@ -16,12 +23,16 @@ export class Review {
   /**
    * DO NOT TOUCH THIS.
    */
+  
+  //attribute of the Review 
   @PrimaryGeneratedColumn()
   id!: number;
 
+  //user can have multiple reviews
   @ManyToOne(() => User, (user) => user.reviews, { cascade: true })
   user!: User;
 
+  //trail can have multiple reviews
   @ManyToOne(() => Trail, (trail) => trail.reviews, { cascade: true })
   trail!: Trail;
 
