eba40dcf358b06e05d6c90e3c7266038cfaec3dd
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2021-04-30 17:56:49 +0800

    Comment HeroSection.js

--- Frontend/src/components/HeroSection.js
+++ Frontend/src/components/HeroSection.js
@@ -1,8 +1,17 @@
+/*	
+	What: This is used to add style to the <HeroSection>, the homepage main component and defiine the functionality of it
+	Who: Tso Ka Wing 1155125488
+	Where: React components for the Homepage to display photos and info
+	Why: We need to define how the text and photo are displayed in the <HeroSection>
+	How: import css style for the  <HeroSection>
+*/
+
 import React from "react";
 import "../App.css";
 import { Button } from "./Button";
 import "./HeroSection.css";
 
+//define function for <HeroSection>
 function HeroSection() {
   return (
     <div className="hero-container">
