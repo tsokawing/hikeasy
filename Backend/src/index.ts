060037192723b3306249ca92a9427ded6d7e90ca
Vectorial1024 <vincentwwy168@gmail.com>
2021-04-30 19:10:22 +0800

    Delete unused/early prototype code segments

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -16,7 +16,7 @@ import { TrailService } from './service/TrailService';
 import { UserService } from './service/UserService';
 import { EventService } from './service/EventService';
 import { ReviewService } from './service/ReviewService';
-import fileUpload, { UploadedFile } from 'express-fileupload';
+import fileUpload from 'express-fileupload';
 import cors from 'cors';
 import passport from 'passport';
 import FirebaseStrategy, { TokenLoader } from 'passport-jwt-firebase';
@@ -53,71 +53,6 @@ app.get('/', async function (req: Request, res: Response) {
     running: true,
     message: 'Hello World!',
   });
-});
-
-// read two number and sum them
-app.get('/demo_sum', async function (req: Request, res: Response) {
-  const firstNumber = req.params.num1;
-  const secondNumber = req.params.num2;
-  const sum = firstNumber + secondNumber;
-  res.send({
-    result: sum,
-  });
-  if (req.files !== undefined) {
-    const test = req.files['test'] ?? [];
-    const test2 = test as UploadedFile;
-    if (test2 !== null) {
-    }
-  }
-});
-
-// receive upload files
-// is always POST
-app.post('/demo_upload', async function (req: Request, res: Response) {
-  try {
-    console.log(
-      '/demo_upload triggered; uploaded files are available under req.files:'
-    );
-    console.log(req.files);
-    if (!req.files) {
-      res.send({
-        status: false,
-        message: 'No file uploaded',
-      });
-    } else {
-      // todo: standardize with an iterface so that we can change <object> to <ModelName>
-      // refer to the UploadedFile type from express-fileupload
-      const data: Array<object> = [];
-
-      // determine if it is single file or multi file
-      // also unify the data types
-      const arrayOfPhotos = Array.isArray(req.files['photos'])
-        ? req.files['photos']
-        : [req.files['photos']];
-
-      //loop all files
-      arrayOfPhotos.forEach((photo) => {
-        //move photo to uploads directory
-        photo.mv('./uploads/' + photo.name);
-
-        //push file details
-        data.push({
-          name: photo.name,
-          mimetype: photo.mimetype,
-          size: photo.size,
-        });
-      });
-
-      //return response
-      res.send({
-        status: true,
-        message: 'Files are uploaded',
-        data: data,
-      });
-    }
-  } catch (err) {
-    res.status(500).send(err);
-  }
 });
 
 // link the different modules to the main file
