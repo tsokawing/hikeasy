b3c071ff73dc94db6b5b5a559484a34a9d051350
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2021-04-30 17:43:09 +0800

    Comment EmphasisButton.js

--- Frontend/src/components/EmphasisButton.js
+++ Frontend/src/components/EmphasisButton.js
@@ -1,3 +1,12 @@
+/*	
+	What: This is used to add style to the <EmphasisButton> and defiine the functionality of it
+	Who: Tso Ka Wing 1155125488
+	Where: React components for the Button in the nav bar
+	Why: We need to set up standard style for the Button for the button in nav bar, so we have to define the style in /componenet directory
+	How: import css style for the <EmphasisButton> componenet using the <Button> of semantic-ui, and use react-router to set router
+*/
+
+//imports
 import React, { Component } from "react";
 import { Button } from "semantic-ui-react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
