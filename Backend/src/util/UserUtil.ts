48e6abd77591030d5c33266bcf1c345a9d1d403d
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2021-04-30 08:51:54 +0800

    Comment UserUtil.ts

--- Backend/src/util/UserUtil.ts
+++ Backend/src/util/UserUtil.ts
@@ -1,3 +1,12 @@
+/*
+  What: This is used to hide private information regarding the /user endpoint
+  Who: Wong Wing Yan 1155125194
+  Where: backend connection
+  Why: Gather all the part to hide private information in /user endpoint to avoid redundanrt code segment
+  How: use typescript to set up the template to notify error
+*/
+
+//imports
 import { User } from '../entity/User';
 
 export class UserUtil {
