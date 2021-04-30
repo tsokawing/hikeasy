6d6d50fd6fabb4a9a44f3e81a6e280a31b185501
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2021-04-30 17:18:02 +0800

    Comment Button.js

--- Frontend/src/components/Button.js
+++ Frontend/src/components/Button.js
@@ -1,11 +1,21 @@
+/*	
+	What: This is used to add style to the <Button> and defiine the functionality of it 
+	Who: Tso Ka Wing 1155125488
+	Where: React component
+	Why: We need to set up standard style for the button, so we have to define the style in /componenet directory
+	How: import css style for the button componenet 
+	*/
+
+//imports
 import React from "react";
 import "./Button.css";
 import { Link } from "react-router-dom";
-
+//standard styles
 const STYLES = ["btn--primary", "btn--outline", "btn--test"];
-
+//standard sizes
 const SIZES = ["btn--medium", "btn--large"];
 
+//export the Button to HikEasy
 export const Button = ({
   children,
   type,
@@ -13,12 +23,13 @@ export const Button = ({
   buttonStyle,
   buttonSize,
 }) => {
+  
   const checkButtonStyle = STYLES.includes(buttonStyle)
     ? buttonStyle
     : STYLES[0];
 
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
-
+  //returning the <Button>
   return (
     // <Link to="/login" className="btn-mobile">
     <button
