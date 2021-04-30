75ea93e86f5232fe3ad64dccc95345e23776a0ee
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2021-04-30 17:29:51 +0800

    Comment CommentSection.js

--- Frontend/src/components/CommentSection.js
+++ Frontend/src/components/CommentSection.js
@@ -1,6 +1,16 @@
+/*	
+	What: This is used to add <Link> to the trailListPage.js upon clicking particular trail, and mapping out the trail
+	Who: Tso Ka Wing 1155125488
+	Where: React components for the /trails endpoint in frontend
+	Why: Upon clicking particular card of the trail, we need to redirect the users 
+	How: import css style for the <CommentSection>
+	*/
+
+//imports
 import React, { Component } from "react";
 import Comments from "./Comments";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
 
 class CommentSection extends Component {
   render() {
